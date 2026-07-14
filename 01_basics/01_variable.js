7de7d1ac4f011e0b5b015d30e7ee97b39b43b414
Indrani neogi <147839930+BELLATRIX29@users.noreply.github.com>
2026-07-14 19:11:17 +0000

    datatype

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const AccountId = 19876
+let accountEmail = "indranineogi@gmail.com"
+var accountPassword = "12345"
+accountCity = "west bangal"
+
+//AccountId = 2// not allowwed
+
+accountEmail = "moniindrani@gmail.com"
+accountPassword = "98765"
+accountCity ="goa"
+console.table ([AccountId,accountEmail,accountPassword,accountCity])
+
+console.log(AccountId);
---
+++ 01_basics/02_datatyes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat as older version javascript to an new version.
+
+//alert (3+3) // not give a output because we dont use browser, we used nodejs
+// number => 2 to the 53 if we have to is use bigger number than we have to use
+// bigint
+// string => "" we prefer to used " "
+// boolean => true/false (yes/no)
+// null => standlone value
+// undefine=> we did not put a vaule iin the box
+// symbol=> unique
+
+// object
+
+console.log(typeof undefined) // data type undefined
+console.log(typeof null) // object
