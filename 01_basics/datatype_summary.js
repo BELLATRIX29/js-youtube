32ded6fdff22f387fc881e561005dce2d09b50fa
Indrani neogi <147839930+BELLATRIX29@users.noreply.github.com>
2026-07-16 09:59:07 +0000

    datatype

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,29 @@
+//premitive
+// its has 7 types : strings, numbers, boolean, null, undefine, symbol, BigInt.
+
+const score = "33" //example of string,data type strings
+const score = 33 // example of numbers, data type number
+const scorevaule = 33.33 //same js did not complecate the numbers.
+const loggedinid = false // boolean, data type boolean
+const currenttmp = null // null, this data type is object
+let emailid = undefined // undefined
+const id = symbol("123")
+const anotherid = symbol("123")
+console.log(id === anotherid); // its didnot change becasue symbol used for a particular unique vaule, data type symbol
+
+const BigInt = 25736824948409408474n
+//and non premitive or reference type
+
+//3 types
+// array, obejects, functions
+const heros = ["oggy", "bheem", "doremon"]; // example of array // data type object
+
+
+let myobj {
+    name = "indrani",
+    age = 25,
+ } // example of objects,data type object
+
+const myFunction = function(){
+    console.log("hello world");
+}// data type function object
