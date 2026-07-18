667b28601f6b3ae325386fb387558d5a301c91b0
Indrani neogi <147839930+BELLATRIX29@users.noreply.github.com>
2026-07-18 20:10:05 +0000

    strings

---
+++ 01_basics/03_strings.js
@@ -0,0 +1,14 @@
+//const name = "indrani"
+//const age = 24
+//console.log(`my name is ${name} and my age is ${age}`);
+
+
+//const gamename = new string('bellatrix')
+
+const newgame = new String('bellatrix')
+let index = 5;
+console.log(`An index of ${index} returns the character ${newgame.at(index)}`);
+index = -4;
+console.log(`An index of ${index} returns the character ${newgame.at(index)}`);
+index1 = -7;
+console.log(`an index of ${index1} return the character ${newgame.at(index1)}`);
