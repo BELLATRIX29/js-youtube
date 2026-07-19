4cd328b728d2df46a5649973f8130935b4724b3f
Indrani neogi <147839930+BELLATRIX29@users.noreply.github.com>
2026-07-19 14:25:38 +0000

    strings

--- 01_basics/03_strings.js
+++ 01_basics/03_strings.js
@@ -5,10 +5,24 @@
 
 //const gamename = new string('bellatrix')
 
-const newgame = new String('bellatrix')
-let index = 5;
-console.log(`An index of ${index} returns the character ${newgame.at(index)}`);
-index = -4;
-console.log(`An index of ${index} returns the character ${newgame.at(index)}`);
-index1 = -7;
-console.log(`an index of ${index1} return the character ${newgame.at(index1)}`);
+//const newgame = new String('bellatrix')
+//let index = 5;
+//console.log(`An index of ${index} returns the character ${newgame.at(index)}`);
+//index = -4;
+//console.log(`An index of ${index} returns the character ${newgame.at(index)}`);
+//index1 = -7;
+//console.log(`an index of ${index1} return the character ${newgame.at(index1)}`);
+const sentnce = "indrani bought 3 mangoes and 2 bananas and keshav bought 5 mangoes and 6 apple from the market yesterday.";
+//const word = "cat";
+
+//console.log(`The word "${word}" ${sentnce.includes(word) ? "is" : "is not"} in the sentence`,);
+//const index = 20;
+//console.log(`the charecter at the index ${index} is ${sentnce.charAt(index)}`);
+//console.log(`character code ${sentnce.charCodeAt(index)} is equal to ${sentnce.charAt(index)}`,);
+//console.log(sentnce.endsWith("yesterday."));
+const searchterm = "mangoes";
+//const searchterm2 = sentnce.indexOf(searchterm);
+
+//console.log(`the index of the first "${searchterm}" is ${searchterm2}`);
+//console.log(`the index of the two "${searchterm}" is ${sentnce.indexOf(searchterm,searchterm2 + 1)}`);
+console.log(`index of the last "${searchterm}" is ${sentnce.lastIndexOf(searchterm)}`);
